fcf7f16808c4f7cf796bb6f73893a8155afc0284
oyatmicah <oyatmicah@gmail.com>
2022-03-16 17:31:08 +0100

    3rd commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import React from 'react';
-import {robots} from './robots';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
+import React, { Component } from "react";
+import { robots } from "./robots";
+import CardList from "./CardList";
+import SearchBox from "./SearchBox";
 
-const App = () => {
-  return (
-    <div className='tc'>
-      <h1>RoboFriends</h1>
-      <SearchBox />
-      <CardList robots={robots}/>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: robots,
+      searchfield: "",
+    };
+  }
+
+  onStateChange = (event) => {
+    this.setState({ searchfield: event.target.value })
+    
+  }
+
+  render() {
+    const filteredRobots = this.state.robots.filter(robots =>{
+      return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    })
+    return (
+      <div className="tc">
+        <h1>RoboFriends</h1>
+        <SearchBox searchChange={this.onStateChange}/>
+        <CardList robots={filteredRobots} />
+      </div>
+    );
+  }
 }
 
 export default App;
